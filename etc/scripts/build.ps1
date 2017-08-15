4761a425382492d9878bce4a9355d7e1a8c14362
Alexander Kozlenko <alexander.kozlenko@live.com>
2017-08-15 21:32:10 +0300

    Update build scripts

--- etc/scripts/build.ps1
+++ etc/scripts/build.ps1
@@ -1,12 +1,6 @@
 [string]$workspace = "$PSScriptRoot\..\.."
 [string]$configuration = "Release"
 
-& dotnet restore "$workspace\src\"
-
-If ($LASTEXITCODE -ne 0) {
-    Throw "Restore command failed with code $LASTEXITCODE"
-}
-
 & dotnet build "$workspace\src\" --configuration $configuration
 
 If ($LASTEXITCODE -ne 0) {
